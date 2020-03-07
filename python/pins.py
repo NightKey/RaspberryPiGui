aaff4703c2b3947f3070be1a9c558b0b15982a2a
Janthó Dávid <janthod@inf.u-szeged.hu>
2020-03-07 01:35:08 +0100

    Created a new class to store all the pins

---
+++ python/pins.py
@@ -0,0 +1,9 @@
+class pins():
+    def __init__(self):
+        self.lamp_pin = 5
+        self.cabinet_pin = 7
+        self.tub_pin = 6
+        self.door_pin = 4
+        self.green_pin = 13
+        self.red_pin = 12
+        self.blue_pin = 18
