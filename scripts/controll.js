9cf8a359f56f3563842e5967ecc3ee0c5a35a0dd
Janthó Dávid <janthod@inf.u-szeged.hu>
2020-03-07 14:02:46 +0100

    Tidied up a little

--- scripts/controll.js
+++ scripts/controll.js
@@ -1,4 +1,36 @@
 window.onload = function(){
+
+    /*Variables*/
+    let brightness = document.getElementById('brightness');
+    let brightness_text = document.getElementById("brightness_number");
+    let bvalue = brightness.value;
+    let modal = document.getElementById('alerts');
+    let error = document.getElementById('error');
+    let message = document.getElementById('message');
+    let error_msg = document.getElementById('error_message');
+    let message_msg = document.getElementById('message_message');
+    let dismiss = document.getElementById('Dismiss');
+    let ok = document.getElementById('Ok');
+    let cancle = document.getElementById('Cancle');
+    let picker = document.getElementById('color');
+    let cabinet = document.getElementById("cabinet");
+    let bath_tub = document.getElementById("bath_tub");
+    let room = document.getElementById("room");
+
+    /*Functions*/
+    show_error = function(msg) {
+        error_msg.innerHTML = msg;
+        error.style.display = 'block';
+        modal.style.display = 'block';
+    }
+
+    show_message = function(msg) {
+        message_msg.innerHTML = msg;
+        message.style.display = 'block';
+        modal.style.display = 'block';
+    }
+
+    /* Connection */
     let connection = new this.WebSocket("ws://127.0.0.1:6969");
     let todo;
 
@@ -23,15 +55,13 @@ window.onload = function(){
         }
     }
 
-    let brightness = document.getElementById('brightness');
-    let brightness_text = document.getElementById("brightness_number");
-    let bvalue = brightness.value;
-
     if (bvalue < 10) {
         bvalue = "0" + bvalue;
     }
 
     brightness_text.innerHTML = bvalue;
+
+    /* Event handlers */
     brightness.addEventListener("input", function() {
         
         let bvalue = brightness.value;
@@ -45,48 +75,26 @@ window.onload = function(){
         connection.send(data);
     }, false);
 
-    let room = document.getElementById("room");
-
     room.addEventListener('change', function() {
         let room_on = room.checked;
         console.log("EventListener called for '"+lights.id+"' with value '"+room_on+"'");
         let data = 'room,'+room_on;
         connection.send(data);
-    });
-
-    let bath_tub = document.getElementById("bath_tub");
+    }, false);
 
     bath_tub.addEventListener('change', function() {
         let bath_tub_on = bath_tub.checked;
         console.log("EventListener called for '"+bath_tub.id+"' with value '"+bath_tub_on+"'");
         let data = 'bath_tub,'+bath_tub_on;
         connection.send(data);
-    });
-
-    let cabinet = document.getElementById("cabinet");
+    }, false);
 
     cabinet.addEventListener('change', function() {
         let cabinet_on = cabinet.checked;
         console.log("EventListener called for '"+cabinet.id+"' with value '"+cabinet_on+"'");
         let data = 'cabinet,'+cabinet_on;
         connection.send(data);
-    });
-
-    let color = this.document.getElementById('selected_color');
-
-    color.addEventListener('change', function() {
-        let selected_color = color.background-color;
-        console.log("EventListener called for '"+color.id+"' with value '"+selected_color+"'");
-    });
-
-    let modal = document.getElementById('alerts');
-    let error = document.getElementById('error');
-    let message = document.getElementById('message');
-    let error_msg = document.getElementById('error_message');
-    let message_msg = document.getElementById('message_message');
-    let dismiss = document.getElementById('Dismiss');
-    let ok = document.getElementById('Ok');
-    let cancle = document.getElementById('Cancle');
+    }, false);
 
     work = function(ansver) {
         console.log('Todo: '+todo+'\tAnsver: '+ansver);
@@ -103,36 +111,31 @@ window.onload = function(){
         }
     }
 
-    show_error = function(msg) {
-        error_msg.innerHTML = msg;
-        error.style.display = 'block';
-        modal.style.display = 'block';
-    }
-
-    show_message = function(msg) {
-        message_msg.innerHTML = msg;
-        message.style.display = 'block';
-        modal.style.display = 'block';
-    }
-
     dismiss.addEventListener('click', function() {
         error_msg.innerHTML = '';
         error.style.display = 'none';
         modal.style.display = 'none';
-    });
+    }, false);
     
     ok.addEventListener('click', function() {
         message_msg.innerHTML = '';
         message.style.display = 'none';
         modal.style.display = 'none';
         work(true);
-    });
+    }, false);
 
     cancle.addEventListener('click', function() {
         message_msg.innerHTML = '';
         message.style.display = 'none';
         modal.style.display = 'none';
         work(false);
-    });
+    }, false);
+
+    picker.addEventListener('change', function(event){
+        picker.value = event.target.value;
+        picker.style.backgroundColor = event.target.value;
+        console.log('Selected color: '+picker.value);
+        connection.send('color,'+event.target.value);
+    }, false);
 
 }
