6094a0f4b5f7e1fd763e08d9c40fcff699c1ab1d
Janthó Dávid <janthod@inf.u-szeged.hu>
2020-03-08 11:51:30 +0100

    fuck me

--- python/updater.py
+++ python/updater.py
@@ -4,6 +4,6 @@ from os import remove
 run('git pull -> update.lg')
 with open('update.lg', 'r') as f:
     c = f.read(-1).split('\n')
-remove('update.log')
+remove('update.lg')
 if len(c) > 2:
     run('sudo shutdown -r now')
