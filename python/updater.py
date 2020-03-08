8bd099e652c1f7a8038b9c2f2836347ea3f278ba
Janthó Dávid <janthod@inf.u-szeged.hu>
2020-03-08 16:34:48 +0100

    Reworked the updater

--- python/updater.py
+++ python/updater.py
@@ -1,9 +1,14 @@
 from os import system as run
 from os import remove
 
-run('git pull -> update.lg')
-with open('update.lg', 'r') as f:
-    c = f.read(-1).split('\n')
-remove('update.lg')
-if len(c) > 2:
-    run('sudo shutdown -r now')
+
+def main():
+    run('git pull --> update.lg')
+    with open('update.lg', 'r') as f:
+        c = f.read(-1).split('\n')
+    remove('update.lg')
+    if len(c) > 2:
+        run('sudo shutdown -r now')
+
+if __name__=='__main__':
+    main()
