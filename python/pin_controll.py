4b2350de5a52ec6007420f05d9b4f5015e8867a8
Janthó Dávid <janthod@inf.u-szeged.hu>
2020-04-08 12:45:02 +0200

    added the pwm controll

--- python/pin_controll.py
+++ python/pin_controll.py
@@ -23,9 +23,12 @@ class controller():
         GPIO.setup(pins.green_pin, GPIO.OUT)                             #Green color
         GPIO.setup(pins.blue_pin, GPIO.OUT)                              #Blue color
         GPIO.setup(pins.fan_controll, GPIO.OUT)                          #Fancontroller
-        self.red = GPIO.PWM(pins.red_pin, 60)
-        self.green = GPIO.PWM(pins.green_pin, 60)
-        self.blue = GPIO.PWM(pins.blue_pin, 60)
+        self.red = GPIO.PWM(pins.red_pin, 100)
+        self.green = GPIO.PWM(pins.green_pin, 100)
+        self.blue = GPIO.PWM(pins.blue_pin, 100)
+        self.red.start(0)
+        self.green.start(0)
+        self.blue.start(0)
         self.status = {
             'brightness':0,
             'room':False,
@@ -48,10 +51,27 @@ class controller():
         self.status['cabinet'] = not bool(GPIO.input(pins.cabinet_pin))
         self.status['fan'] = not bool(GPIO.input(pins.fan_controll))
 
+    def translate(self, value, inmin, inmax, outmin, outmax):
+        """
+        Translates the value from range inmin - inmax into the range outmin - outmax. Returns an integer!
+        Input: Value - The value to be translated; inmin, inmax - The input value's minimum, and maximum possible value; outmin, outmax - The return values minimum and maximum possible value.
+        Output: Nothing
+        Return: Float rounded to 2 digits. The value is between outmin and outmax, and is the translated value of the input value.
+        """
+        inspan = inmax - inmin
+        outspan = outmax - outmin
+        scaled = float(value - inmin) / float(inspan)
+        return round(outmin + (scaled * outspan), 2)
 
     def brightness(self, value):
         verbose(f"Incoming for brightness {value}", 'PINS')
         self.status['brightness'] = int(value)
+        self.set_leds()
+
+    def set_leds(self):
+        self.red.ChangeDutyCycle((self.translate(self.status['color'][0], 0, 255, 0, 100) / self.translate(self.status['brightness'], 0, 12, 0, 100)))
+        self.red.ChangeDutyCycle((self.translate(self.status['color'][1], 0, 255, 0, 100) / self.translate(self.status['brightness'], 0, 12, 0, 100)))
+        self.red.ChangeDutyCycle((self.translate(self.status['color'][2], 0, 255, 0, 100) / self.translate(self.status['brightness'], 0, 12, 0, 100)))
 
     def room(self, is_on):
         is_on = (is_on == 'true')
@@ -64,19 +84,33 @@ class controller():
         verbose("The bath tub lights should {}be on!".format('' if (is_on) else 'not '), 'PINS')
         GPIO.output(pins.tub_pin, (GPIO.HIGH if is_on else GPIO.LOW))
         self.status['bath_tub'] = GPIO.input(pins.tub_pin)
+        if not self.status['cabinet']:
+            if not is_on:
+                self.red.ChangeDutyCycle(0)
+                self.green.ChangeDutyCycle(0)
+                self.blue.ChangeDutyCycle(0)
+            else:
+                self.set_leds()
 
     def cabinet(self, is_on):
         is_on = (is_on == 'true')
         verbose("The cabinet lights should {}be on!".format('' if (is_on) else 'not '), 'PINS')
         GPIO.output(pins.cabinet_pin, (GPIO.HIGH if is_on else GPIO.LOW))
         self.status['cabinet'] = GPIO.input(pins.cabinet_pin)
-
+        if not self.status['bath_tub']:
+            if not is_on:
+                self.red.ChangeDutyCycle(0)
+                self.green.ChangeDutyCycle(0)
+                self.blue.ChangeDutyCycle(0)
+            else:
+                self.set_leds()
 
     def color(self, color_v):
         color_v = color_v.replace('#', '')
         color_v = [int(color_v[:2], 16), int(color_v[2:4], 16), int(color_v[4:], 16)]
         verbose(f"The color of the led's should be #{color_v}", 'PINS')
         self.status['color'] = color_v
+        self.set_leds()
 
     def fan(self, status=None):
         if status == None:
