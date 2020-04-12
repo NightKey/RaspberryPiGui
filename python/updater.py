61ea56e5a1ef9800d3e8228222740efdab698a64
Janthó Dávid <janthod@inf.u-szeged.hu>
2020-04-12 16:49:52 +0200

    Removes the logfile to the time of updating It can cause errors in git pull

--- python/updater.py
+++ python/updater.py
@@ -1,12 +1,19 @@
 from os import system as run
-from os import remove
+import os
 
 
 def main():
+    if os.path.exists('RaspberryPiServerLog.lg'):
+        os.rename('RaspberryPiServerLog.lg', '/tmp/RaspberryPiServerLog.lg')
+    update()
+    if os.path.exists('/tmp/RaspberryPiServerLog.lg'):
+        os.rename('/tmp/RaspberryPiServerLog.lg', 'RaspberryPiServerLog.lg')
+
+def update():
     run('git pull > update.lg')
     with open('update.lg', 'r') as f:
         c = f.read(-1).split('\n')
-    remove('update.lg')
+    os.remove('update.lg')
     if len(c) > 2:
         run('sudo shutdown -r now')
 
