95c04c041691e54b61d302450a22a0561cca04f3
Janthó Dávid <janthod@inf.u-szeged.hu>
2020-04-13 17:18:30 +0200

    Added encoder support

--- python/pins.py
+++ python/pins.py
@@ -1,4 +1,6 @@
 class pins():
+    """This class contains all of the pin numbers the different things will be connected to.
+    """
     def __init__(self):
         self.lamp_pin = 5
         self.cabinet_pin = 7
@@ -8,3 +10,11 @@ class pins():
         self.red_pin = 12
         self.blue_pin = 23
         self.fan_controll = 19
+
+class encoder():
+    """This class contains the rotory encoder's different pin numbers.
+    """
+    def __init__(self):
+        self.turn_left = -1
+        self.turn_right = -1
+        self.push = -1
