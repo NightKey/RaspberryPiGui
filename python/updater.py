d8fc4909008218e2bc56ec041e84371d613e745c
Janthó Dávid <janthod@inf.u-szeged.hu>
2020-04-21 14:07:27 +0200

    Moves away the status for update

--- python/updater.py
+++ python/updater.py
@@ -5,17 +5,22 @@ import os
 def main():
     if os.path.exists('RaspberryPiServerLog.lg'):
         os.rename('RaspberryPiServerLog.lg', '/tmp/RaspberryPiServerLog.lg')
-    update()
+    if os.path.exists('status.json'):
+        os.rename('status.json', '/tmp/status.json')
+    c = update()
     if os.path.exists('/tmp/RaspberryPiServerLog.lg'):
         os.rename('/tmp/RaspberryPiServerLog.lg', 'RaspberryPiServerLog.lg')
+    if os.path.exists('/tmp/status.json'):
+        os.rename('/tmp/status.json', 'status.json')
+    if len(c) > 2:
+        run('sudo shutdown -r now')
 
 def update():
     run('git pull > update.lg')
     with open('update.lg', 'r') as f:
         c = f.read(-1).split('\n')
     os.remove('update.lg')
-    if len(c) > 2:
-        run('sudo shutdown -r now')
+    return c
 
 if __name__=='__main__':
     main()
