71e39763606be21937d15fc739a93b0a4ab57ec1
Night Key <davidjantho@gmail.com>
2020-06-26 22:03:10 +0200

    Now only creates a file named 'Update_required' to signal, that an update was finished

--- python/updater.py
+++ python/updater.py
@@ -1,13 +1,15 @@
 from os import system as run
-import os
+from os import remove
+from sys import stderr
 
 def update():
     run('git pull > update.lg')
     with open('update.lg', 'r') as f:
         c = f.read(-1).split('\n')
-    os.remove('update.lg')
+    remove('update.lg')
     if len(c) > 2:
-        run('sudo shutdown -r now')
+       with open('Update_required', 'w') as f:
+                pass
 
 if __name__=='__main__':
     update()
