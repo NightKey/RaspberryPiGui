e01a4d9e61f47a531450b8474171863caae92111
Night Key <davidjantho@gmail.com>
2020-06-27 04:23:37 +0200

    Now can kill the server, and restart it. Also sends last argument to server (only two used -v/-d)

--- python/runner.py
+++ python/runner.py
@@ -3,6 +3,8 @@ from platform import system
 from version import version_info
 from os import system as run
 from os import path, remove
+from sys import argv
+from time import sleep
 
 interpreter = 'python' if system() == 'Windows' else 'python3'
 current = None
@@ -11,38 +13,54 @@ def read_version():
     global current
     with open('../version', 'r') as f:
         current = version_info(f.read(-1).split('\n'))
-print(f'Current version: {current}')
-server = subprocess.Popen([interpreter, 'server.py'])
-while server.poll() is None:
-    if path.exists('Ready'):
-        remove('Ready')
-    if path.exists('Update_required'):
-        remove('Update_required')
-        print('Updated!')
-        with open('../version', 'r') as f:
-            tmp = version_info(f.read(-1).split('\n'))
-        ret = current.check_against(tmp)
-        if ret == 0:
-            continue
-        if ret == 1 or ret == 3:
-            server.kill()
-            while server.poll() is None:
-                pass
-            server = subprocess.Popen([interpreter, 'server.py'])
-            t = True
-            while t:
-                try:
-                    for line in server.stdout:
+
+def main():
+    read_version()
+    print(f'Current version: {current}')
+    arg = argv[-1] if argv[-1] != 'runner.py' else ''
+    server = subprocess.Popen([interpreter, 'server.py', arg])
+    while server.poll() is None:
+        try:
+            if path.exists('Ready'):
+                remove('Ready')
+            if path.exists('KILL'):
+                remove('KILL')
+                server.kill()
+            if path.exists('Update_required'):
+                remove('Update_required')
+                print('Updated!')
+                with open('../version', 'r') as f:
+                    tmp = version_info(f.read(-1).split('\n'))
+                ret = current.check_against(tmp)
+                if ret == 0:
+                    continue
+                if ret == 1 or ret == 3:
+                    server.kill()
+                    while server.poll() is None:
+                        pass
+                    server = subprocess.Popen([interpreter, 'server.py', arg])
+                    t = True
+                    while t:
                         if path.exists('Ready'):
                             remove('Ready')
                             t = False
                             break
-                except TypeError:
-                    pass
-        if ret == 2 or ret == 3:
-            with open('Refresh', 'w') as f:
-                pass
-        if ret == 4:
-            run("sudo shutdown -r now")
-        read_version()
-        print(f'Current version: {current}')
+                if ret == 2 or ret == 3:
+                    with open('Refresh', 'w') as f:
+                        pass
+                if ret == 4:
+                    run("sudo shutdown -r now")
+                read_version()
+                print(f'Current version: {current}')
+        except:
+            pass
+        finally:
+            sleep(0.2)
+
+if __name__ == '__main__':
+    while True:
+        main()
+        print('Server killed!')
+        ansv = str(input('Do you want to restart the server? ([Y]/N) ') or 'Y')
+        if ansv.upper() == 'N':
+            break
