e9bf7608e63264c836e9f9deed8ee8659a893ee9
Night <davidjantho@gmail.com>
2022-07-30 12:28:44 +0200

    Fixed an error with the version file

--- python/version_creator.py
+++ python/version_creator.py
@@ -12,24 +12,28 @@ try:
         data = f.read(-1).split('\n')
         print('Inported')
 except Exception as ex:
-    try: 
+    try:
         with open('../version', 'r') as f:
             data = f.read(-1).split('\n')
             print('Inported')
     except:
         print(f'{type(ex)} --> {ex}')
-        data = [f'current 0.0.0', 'server_untill 0.0.0', 'browser_untill 0.0.0', 'total 0.0.0']
+        data = [f'current 0.0.0', 'server_untill 0.0.0',
+                'browser_untill 0.0.0', 'total 0.0.0']
 print(f"Last version: {data[0].split(' ')[1]}")
 current = input('Type in the new version (x.y.z): ')
 tmp = data[0].split(' ')[-1]
 data[0] = f'current {current}'
-ansv = str(input('Did anything change with the server since the last update? (Y/[N]) ') or 'N')
+ansv = str(input(
+    'Did anything change with the server since the last update? (Y/[N]) ') or 'N')
 if ansv.upper() == 'Y':
     data[1] = f'server_untill {tmp}'
-ansv = str(input('Did anything change with the browser since the last update? (Y/[N]) ') or 'N')
+ansv = str(input(
+    'Did anything change with the browser since the last update? (Y/[N]) ') or 'N')
 if ansv.upper() == 'Y':
     data[2] = f'browser_untill {tmp}'
-ansv = str(input('Were there anything requiriing a system restart? (For example changes to the runner.) (Y/[N]) ') or 'N')
+ansv = str(input(
+    'Were there anything requiriing a system restart? (For example changes to the runner.) (Y/[N]) ') or 'N')
 if ansv.upper() == 'Y':
     data[3] = f'total {tmp}'
 with open('version', 'w') as f:
